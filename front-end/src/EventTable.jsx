343daeea501e216b00f304a5d7adcb52aa823c9d
Társila Samille <tarsillasamile@gmail.com>
2024-04-08 13:40:58 -0300

    a
    a

--- front-end/src/EventTable.jsx
+++ front-end/src/EventTable.jsx
@@ -1,4 +1,3 @@
-// EventTable.jsx
 import React, { useState, useEffect } from "react";
 import {
   Table,
--- front-end/src/context/AuthContext.js
+++ front-end/src/context/AuthContext.js
@@ -1,4 +1,3 @@
-// src/context/AuthContext.js
 import React, { createContext, useState, useContext } from "react";
 
 const AuthContext = createContext();
