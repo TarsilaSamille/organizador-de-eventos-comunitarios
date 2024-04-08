d25e5e6480d698f4d9bb4edd626d13800c627d26
Társila Samille <tarsillasamile@gmail.com>
2024-04-08 13:16:57 -0300

    a
    a

--- backend/api/index.js
+++ backend/api/index.js
@@ -9,7 +9,6 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware para analisar corpos JSON
 app.use(cors());
 app.use(express.json());
 
