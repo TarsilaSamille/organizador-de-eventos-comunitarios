2aada6d44b6d956412e8ff5e0f7acc6a57b3e7a3
Társila Samille <tarsillasamile@gmail.com>
2024-04-10 19:07:55 -0300

    a
    a

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,20 +3,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
-const cors = require("cors");
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
 app.use(express.json());
 
-const uri = `mongodb://localhost:27017/eventos-comunitarios`;
-// const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.3s7ir2u.mongodb.net/eventos-comunitarios?retryWrites=true&w=majority&authSource=admin`;
+// const uri = `mongodb://localhost:27017/eventos-comunitarios`;
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.3s7ir2u.mongodb.net/eventos-comunitarios?retryWrites=true&w=majority&authSource=admin`;
 
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -24,7 +18,6 @@ mongoose
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
 
 app.use(
   session({
@@ -32,6 +25,15 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false },
+  })
+);
+
+const frontOrigin = `${process.env.FRONT_ORIGIN}`;
+const cors = require("cors");
+app.use(
+  cors({
+    origin: frontOrigin,
+    credentials: true,
   })
 );
 
